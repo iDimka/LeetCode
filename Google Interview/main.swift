bf656e0573d6a8a2f42d6ff9c4a6e63bf2d05432
Dmitry Sazanovich <idimka@me.com>
2020-01-16 11:27:57 -0500

    Initial Commit

---
+++ Google Interview/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Google Interview
+//
+//  Created by Dmitry Sazanovich on 1/16/20.
+//  Copyright © 2020 idimka. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
