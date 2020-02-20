1b2bc945f2253266b327e1aa2858d71f773284ce
Dmitry Sazanovich <idimka@me.com>
2020-02-19 22:20:58 -0800

    rotate image/matrix

--- Google Interview/main.swift
+++ Google Interview/main.swift
@@ -1,12 +1,4 @@
-//
-//  main.swift
-//  Google Interview
-//
-//  Created by Dmitry Sazanovich on 1/16/20.
-//  Copyright © 2020 idimka. All rights reserved.
-//
-
 import Foundation
 
-RotateArray.test()
+Rotate_Image.test()
 
