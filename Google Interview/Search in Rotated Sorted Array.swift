96a5297c9173d2662b735cb1e4d6b84689d34544
Dmitry Sazanovich <idimka@me.com>
2020-03-05 21:20:07 -0800

    Search in Rotated Sorted Array

--- Google Interview/Search in Rotated Sorted Array.swift
+++ Google Interview/Search in Rotated Sorted Array.swift
@@ -49,28 +49,35 @@ class BinarySearch: NSObject {
     }
     
     class func searcInRotatedArray<T: Comparable>(in array: [T], value: T) -> Int {
+        guard array.count > 0 else { return -1 }
         guard let rotationPoint = findRotation(in: array) else { return -1 } // something went wrong
         
         if value == array[rotationPoint] {
             return rotationPoint
+        } else if array[0] == value {
+            return 0
         }
         
-        if array[rotationPoint] < value {
+        if rotationPoint == 0 {
+            return findIndex(of: value, in: array)
+        } else if array[0] < value {
             return findIndex(of: value, in: array[0..<rotationPoint].array)
         } else {
-            return findIndex(of: value, in: array[rotationPoint..<array.count].array)
+            let result = findIndex(of: value, in: array[rotationPoint..<array.count].array)
+            return result == -1 ? -1 : result + rotationPoint
         }
     }
     
     class func test() {
         let input = [0, 1, 2, 3, 4, 5, 6]
         print(BinarySearch.findIndex(of: 3, in: input))
-        
+//        
         let rotatedaArray = [4, 5, 6, 8, 0, 1, 2, 3]
         print(BinarySearch.findRotation(in: rotatedaArray) ?? -1)
-        
+//        
         print("Search rotated index")
-        print(searcInRotatedArray(in: rotatedaArray, value: 4))
+        print(searcInRotatedArray(in: [5,1,3], value: 3))
+        print(searcInRotatedArray(in: [4,5,6,7,0,1,2], value: 3))
         print(searcInRotatedArray(in: rotatedaArray, value: 0))
         print(searcInRotatedArray(in: rotatedaArray, value: 5))
     }
