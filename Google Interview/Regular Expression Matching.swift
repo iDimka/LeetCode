cd0007e847f565de7d1b2399f8379b66476758dd
Dmitry Sazanovich <idimka@me.com>
2020-05-10 22:26:13 -0700

    permuteUnique

--- Google Interview/Regular Expression Matching.swift
+++ Google Interview/Regular Expression Matching.swift
@@ -53,6 +53,23 @@ import Cocoa
  s = "mississippi"
  p = "mis*is*p*."
  Output: false
+ 
+ class Solution {
+     public boolean isMatch(String text, String pattern) {
+         if (pattern.isEmpty()) {
+             return text.isEmpty();
+         }
+
+         boolean first_match = (!text.isEmpty() && (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
+
+         if (pattern.length() > 1 && pattern.charAt(1) == '*'){
+             return (isMatch(text, pattern.substring(2)) || (first_match && isMatch(text.substring(1), pattern)));
+         } else {
+             return first_match && isMatch(text.substring(1), pattern.substring(1));
+         }
+     }
+ }
+ 
  */
 
 class Regular_Expression_Matching: NSObject {
