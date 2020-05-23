287df88090923475396afbff4c2cf2dbc68081e5
Dmitry Sazanovich <idimka@me.com>
2020-05-23 15:38:09 -0700

    Longest Palindromic Substring (fixed)

--- Google Interview/Longest Palindromic Substring.swift
+++ Google Interview/Longest Palindromic Substring.swift
@@ -30,70 +30,76 @@ import Cocoa
  If we use brute-force and check whether for every start and end position a substring is a palindrome we have O(n^2) start - end pairs and O(n) palindromic checks. Can we reduce the time for palindromic checks to O(1) by reusing some previous computation.
  */
 class Longest_Palindromic_Substring: NSObject {
-  func longestPalindrome(_ s: String) -> String {
-    var dp = Array(repeating: Array(repeating: 0, count: s.count), count: s.count)
-    var max = Int.min
-    var result = ""
+  func longestPalindrome(_ string: String) -> String {
+    guard string.count > 1 else { return string }
     
-    for ind in 0..<s.count {
-      dp[ind][ind] = 1
+    let array = string.map { String($0) }
+    
+    var dp = Array(repeating: Array(repeating: false, count: array.count), count: array.count)
+    var max = Int.min
+    
+    for ind in 0..<array.count {
+      dp[ind][ind] = true
     }
     
     dp.printMatrix()
     
-    for ind in 0..<s.count - 1 {
-      let startString = s[s.index(s.startIndex, offsetBy: ind)]
-      let endString = s[s.index(s.startIndex, offsetBy: ind + 1)]
+    var startSubstring = 0
+    var endSubstring = 0
+    
+    for ind in 0..<array.count - 1 {
+      let startString = array[array.index(array.startIndex, offsetBy: ind)]
+      let endString = array[array.index(array.startIndex, offsetBy: ind + 1)]
       
-      dp[ind][ind + 1] = startString == endString ? 2 : 1
-      if dp[ind][ind + 1] > max {
-        max = Swift.max(max, dp[ind][ind + 1])
-        result = String(s[s.index(s.startIndex, offsetBy: ind)...s.index(s.startIndex, offsetBy: ind+1)])
+      dp[ind][ind + 1] = startString == endString
+      if startString == endString, 2 > max {
+        max = Swift.max(max, 2)
+        startSubstring = ind
+        endSubstring = ind + 1
       }
     }
     
-    dp.printMatrix()
+    guard array.count > 2 else {
+      return endSubstring - startSubstring > 0 ? String(string[string.index(string.startIndex, offsetBy: startSubstring)...string.index(string.startIndex, offsetBy: endSubstring)]) : string[string.startIndex].string
+    }
     
-    for index in 3...s.count {
-      for start in 0..<s.count - index + 1 {
+    dp.printMatrix()
+    for index in 3...array.count {
+      for start in 0..<array.count - index + 1 {
         let end = start + index - 1
-        let startString = s[s.index(s.startIndex, offsetBy: start)]
-        let endString = s[s.index(s.startIndex, offsetBy: end)]
+        let startString = array[start]
+        let endString = array[end]
         
-        if startString == endString {
-          dp[start][end] = dp[start + 1][end - 1] + 2
-          if dp[start][end] > max {
-            max = Swift.max(max, dp[start][end])
-            result = String(s[s.index(s.startIndex, offsetBy: start)...s.index(s.startIndex, offsetBy: end)])
+        if startString == endString, dp[start + 1][end - 1] {
+          dp[start][end] = true
+          if end - start + 1 > max {
+            max = end - start + 1
+            startSubstring = start
+            endSubstring = end
           }
-        } else {
-          dp[start][end] = Swift.max(dp[start + 1][end], dp[start][end - 1])
         }
-        print("\(start)-\(end) "+startString.string+endString.string)
+        print("\(start)-\(end) "+startString+endString)
         dp.printMatrix()
       }
     }
     
-    print(dp[0][s.count - 1])
+    print(dp[0][array.count - 1])
     
-    
-    return result.count > 0 ? result : s[s.startIndex].string
+    return endSubstring - startSubstring > 0 ? String(string[string.index(string.startIndex, offsetBy: startSubstring)...string.index(string.startIndex, offsetBy: endSubstring)]) : string[string.startIndex].string
   }
   
   class func test() {
     let solution = Longest_Palindromic_Substring()
-    print(solution.longestPalindrome("ababa"))
+    print(solution.longestPalindrome("bb"))
   }
 }
 
 extension Array {
   func printMatrix() {
-    //    guard first is Array else { print("not 2d"); return }
-    
     for arr in self {
-      guard let arr = arr as? [Int] else { return }
+      guard let arr = arr as? [Bool] else { return }
       
-      print(arr, separator: "", terminator: "\n")
+      print(arr.map { Int($0 ? 1 : 0) }, separator: "", terminator: "\n")
     }
   }
 }
